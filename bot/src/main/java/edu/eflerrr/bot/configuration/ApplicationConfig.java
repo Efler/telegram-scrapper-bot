8960717d379c6aa0942fd28aad6dd5a2a794f188
eflerrr <cool.yan_sv@mail.ru>
2024-02-13 16:29:38 +0300

    configuration for available sites added

--- bot/src/main/java/edu/eflerrr/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/eflerrr/bot/configuration/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package edu.eflerrr.bot.configuration;
 
 import jakarta.validation.constraints.NotEmpty;
+import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -8,6 +9,8 @@ import org.springframework.validation.annotation.Validated;
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotEmpty
-    String telegramToken
+    String telegramToken,
+    @NotEmpty
+    List<String> availableSites
 ) {
 }
