c8e02cc5ba80674856405b391410d1ea44530c7f
eflerrr <cool.yan_sv@mail.ru>
2024-02-24 00:12:42 +0300

    base-api-urls configuration added

--- scrapper/src/main/java/edu/eflerrr/configuration/ApplicationConfig.java
+++ scrapper/src/main/java/edu/eflerrr/configuration/ApplicationConfig.java
@@ -3,14 +3,24 @@ package edu.eflerrr.configuration;
 import jakarta.validation.constraints.NotNull;
 import java.time.Duration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 @ConfigurationProperties(prefix = "app", ignoreUnknownFields = false)
 public record ApplicationConfig(
     @NotNull
+    Api api,
+    @Bean
+    @NotNull
     Scheduler scheduler
 ) {
+    public record Api(
+        String githubBaseUrl,
+        String stackoverflowBaseUrl
+    ) {
+    }
+
     public record Scheduler(boolean enable, @NotNull Duration interval, @NotNull Duration forceCheckDelay) {
     }
 }
