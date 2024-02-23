224f2557b74c64c3dc8cfa1369d8bf9bef671106
eflerrr <cool.yan_sv@mail.ru>
2024-02-24 00:14:33 +0300

    scheduled link updater done

---
+++ scrapper/src/main/java/edu/eflerrr/scheduler/LinkUpdateScheduler.java
@@ -0,0 +1,20 @@
+package edu.eflerrr.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+@ConditionalOnProperty(value = "app.scheduler.enable", havingValue = "true")
+@Slf4j
+public class LinkUpdateScheduler {
+
+    @Scheduled(fixedRateString = "#{@scheduler.interval}")
+    public void update() {
+        log.debug("Updating links...");
+    }
+
+}
