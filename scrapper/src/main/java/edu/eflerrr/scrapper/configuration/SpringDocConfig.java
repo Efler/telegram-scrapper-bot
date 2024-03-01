3daff1783ce2d68e7c4bae05cc785b849e78d9ef
eflerrr <cool.yan_sv@mail.ru>
2024-03-02 02:29:11 +0300

    SpringDocConfig added for Swagger UI description

---
+++ scrapper/src/main/java/edu/eflerrr/scrapper/configuration/SpringDocConfig.java
@@ -0,0 +1,20 @@
+package edu.eflerrr.scrapper.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    OpenAPI apiInfo() {
+        return new OpenAPI()
+            .info(
+                new Info()
+                    .title("Scrapper API")
+                    .version("1.0.0")
+            );
+    }
+}
