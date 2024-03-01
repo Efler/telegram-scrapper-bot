74c8a46de40fdc55de133114eeccef867ee42a07
eflerrr <cool.yan_sv@mail.ru>
2024-03-02 02:26:56 +0300

    custom exceptions are made for scrapper RestControllerAdvice

---
+++ scrapper/src/main/java/edu/eflerrr/scrapper/exception/DuplicateRegistrationException.java
@@ -0,0 +1,7 @@
+package edu.eflerrr.scrapper.exception;
+
+public class DuplicateRegistrationException extends Exception {
+    public DuplicateRegistrationException(String message) {
+        super(message);
+    }
+}
---
+++ scrapper/src/main/java/edu/eflerrr/scrapper/exception/LinkNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.eflerrr.scrapper.exception;
+
+public class LinkNotFoundException extends Exception {
+    public LinkNotFoundException(String message) {
+        super(message);
+    }
+}
