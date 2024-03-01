49c840de54d050b069cd5574f9cafdc039464b35
eflerrr <cool.yan_sv@mail.ru>
2024-03-02 01:17:59 +0300

    refactoring TgBot messages after code review + rebasing readme commits from main

--- bot/src/main/java/edu/eflerrr/bot/command/handler/impl/TrackCommandHandler.java
+++ bot/src/main/java/edu/eflerrr/bot/command/handler/impl/TrackCommandHandler.java
@@ -1,31 +1,34 @@
 package edu.eflerrr.bot.command.handler.impl;
 
 import com.pengrad.telegrambot.model.Update;
-import edu.eflerrr.bot.client.ScrapperClient;
 import edu.eflerrr.bot.command.handler.CommandHandler;
 import edu.eflerrr.bot.configuration.ApplicationConfig;
-import edu.eflerrr.bot.exception.DuplicateLinkPostException;
-import edu.eflerrr.bot.exception.TgChatNotExistException;
+import edu.eflerrr.bot.repository.BotRepository;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import static edu.eflerrr.bot.message.BotMessage.SITE_ERROR;
-import static edu.eflerrr.bot.message.BotMessage.TRACK_COMMAND_EXISTING_URL;
-import static edu.eflerrr.bot.message.BotMessage.TRACK_COMMAND_FORMAT_ERROR;
-import static edu.eflerrr.bot.message.BotMessage.TRACK_COMMAND_SUCCESS;
-import static edu.eflerrr.bot.message.BotMessage.URL_ERROR;
-import static edu.eflerrr.bot.message.BotMessage.USER_NOT_FOUND_ERROR;
+import static edu.eflerrr.bot.command.message.BotMessage.SITE_ERROR;
+import static edu.eflerrr.bot.command.message.BotMessage.TRACK_COMMAND_EXISTING_URL;
+import static edu.eflerrr.bot.command.message.BotMessage.TRACK_COMMAND_FORMAT_ERROR;
+import static edu.eflerrr.bot.command.message.BotMessage.TRACK_COMMAND_SUCCESS;
+import static edu.eflerrr.bot.command.message.BotMessage.URL_ERROR;
+import static edu.eflerrr.bot.command.message.BotMessage.USER_NOT_FOUND_ERROR;
 
 @Component
-@RequiredArgsConstructor
 public class TrackCommandHandler implements CommandHandler {
     private final String name = "/track";
     private final String description = "Начать отслеживание ссылки";
+    private final BotRepository repository;
     private final ApplicationConfig config;
-    private final ScrapperClient scrapperClient;
+
+    @Autowired
+    public TrackCommandHandler(BotRepository repository, ApplicationConfig config) {
+        this.repository = repository;
+        this.config = config;
+    }
 
     @Override
     public String getCommandName() {
@@ -48,8 +51,8 @@ public class TrackCommandHandler implements CommandHandler {
 
     @Override
     public String handle(Update update) {
-        String answer;
         var command = update.message().text();
+        String answer;
         if (!checkFormat(command)) {
             throw new IllegalArgumentException("Invalid command format!");
         }
@@ -58,22 +61,22 @@ public class TrackCommandHandler implements CommandHandler {
         } else {
             URL url;
             try {
-                url = URI.create(command.substring(name.length() + 1)).toURL();
-            } catch (IllegalArgumentException | MalformedURLException ex) {
+                url = new URI(command.substring(name.length() + 1)).toURL();
+            } catch (IllegalArgumentException | MalformedURLException | URISyntaxException ex) {
                 return URL_ERROR;
             }
             if (!checkUrlSupport(url)) {
                 answer = SITE_ERROR;
             } else {
+                var chatId = update.message().chat().id();
                 try {
-                    scrapperClient.trackLink(update.message().chat().id(), url.toURI());
-                    answer = TRACK_COMMAND_SUCCESS;
-                } catch (DuplicateLinkPostException ex) {
-                    answer = TRACK_COMMAND_EXISTING_URL;
-                } catch (TgChatNotExistException ex) {
-                    answer = USER_NOT_FOUND_ERROR;
-                } catch (URISyntaxException ex) {
-                    throw new RuntimeException(ex.getMessage());
+                    if (repository.trackLink(chatId, url)) {
+                        answer = TRACK_COMMAND_SUCCESS;
+                    } else {
+                        answer = TRACK_COMMAND_EXISTING_URL;
+                    }
+                } catch (IllegalArgumentException e) {
+                    return USER_NOT_FOUND_ERROR;
                 }
             }
         }
