900277bd89e55135d7f396f4b1513c8c83b53189
eflerrr <cool.yan_sv@mail.ru>
2024-03-02 01:17:59 +0300

    refactoring TgBot messages after code review + rebasing readme commits from main

--- bot/src/main/java/edu/eflerrr/bot/command/handler/impl/HelpCommandHandler.java
+++ bot/src/main/java/edu/eflerrr/bot/command/handler/impl/HelpCommandHandler.java
@@ -3,20 +3,23 @@ package edu.eflerrr.bot.command.handler.impl;
 import com.pengrad.telegrambot.model.Update;
 import edu.eflerrr.bot.command.handler.CommandHandler;
 import edu.eflerrr.bot.command.list.CommandHandlerList;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-import static edu.eflerrr.bot.message.BotMessage.HELP_COMMAND_EMPTY_LIST_ERROR;
-import static edu.eflerrr.bot.message.BotMessage.HELP_COMMAND_SUCCESS_HEADER;
+import static edu.eflerrr.bot.command.message.BotMessage.HELP_COMMAND_EMPTY_LIST_ERROR;
+import static edu.eflerrr.bot.command.message.BotMessage.HELP_COMMAND_SUCCESS_HEADER;
 
 @Component
-@RequiredArgsConstructor
 public class HelpCommandHandler implements CommandHandler {
     private final String name = "/help";
     private final String description = "Вывести окно с командами";
     private final ApplicationContext context;
+
+    @Autowired
+    public HelpCommandHandler(ApplicationContext context) {
+        this.context = context;
+    }
 
     @Override
     public String getCommandName() {
@@ -33,13 +36,6 @@ public class HelpCommandHandler implements CommandHandler {
         return command != null && command.equals(name);
     }
 
-    public String toMarkdownStyle(String word) {
-        return word.replaceAll(
-            "([" + Pattern.quote("_*[]()~`><#+-=|{}.!") + "])",
-            "\\\\$1"
-        );
-    }
-
     @Override
     public String handle(Update update) {
         if (!checkFormat(update.message().text())) {
@@ -50,12 +46,7 @@ public class HelpCommandHandler implements CommandHandler {
             return HELP_COMMAND_EMPTY_LIST_ERROR;
         } else {
             return HELP_COMMAND_SUCCESS_HEADER + commandHandlers.stream()
-                .map((h) -> String.format(
-                        "*%s* \\-\\> _%s_",
-                        toMarkdownStyle(h.getCommandName()),
-                        toMarkdownStyle(h.getCommandDescription())
-                    )
-                )
+                .map((h) -> String.format("*%s* \\-\\> _%s_", h.getCommandName(), h.getCommandDescription()))
                 .collect(Collectors.joining("\n"));
         }
     }
