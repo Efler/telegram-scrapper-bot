e8b71071f01dc66906979281289d67f19c7ba933
eflerrr <cool.yan_sv@mail.ru>
2024-03-02 02:27:50 +0300

    Bot Client done

---
+++ scrapper/src/main/java/edu/eflerrr/scrapper/client/BotClient.java
@@ -0,0 +1,39 @@
+package edu.eflerrr.scrapper.client;
+
+import edu.eflerrr.scrapper.client.dto.request.SendUpdateRequest;
+import edu.eflerrr.scrapper.client.dto.response.BotErrorResponse;
+import java.net.URI;
+import java.util.List;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class BotClient {
+    private final WebClient webClient;
+    public final String defaultApiUrl = "localhost:8090";
+
+    public BotClient(String baseApiUrl) {
+        this.webClient = WebClient.create(
+            baseApiUrl == null || baseApiUrl.isEmpty()
+                ? defaultApiUrl
+                : baseApiUrl);
+    }
+
+    public void sendUpdate(Long id, URI url, String description, List<Long> tgChatIds) {
+        var request = new SendUpdateRequest(id, url, description, tgChatIds);
+        var botResponse = webClient.post()
+            .uri("/updates")
+            .bodyValue(request)
+            .exchangeToMono(response -> {
+                if (!response.statusCode().is2xxSuccessful()) {
+                    return response.bodyToMono(BotErrorResponse.class);
+                }
+                return Mono.empty();
+            })
+            .block();
+        if (botResponse != null) {
+            throw new RuntimeException(
+                "Error occurred during sendUpdate in BotClient! ErrorResponse: " + botResponse
+            );
+        }
+    }
+}
