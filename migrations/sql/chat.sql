b275614f0f5b694babc36a7d71531c62d41910ce
eflerrr <cool.yan_sv@mail.ru>
2024-03-07 01:40:25 +0300

    sql-formatted changesets done for liquibase migrations

---
+++ migrations/sql/chat.sql
@@ -0,0 +1,10 @@
+-- liquibase formatted sql
+
+-- changeset eflerrr:init_chat_table
+CREATE TABLE Chat
+(
+    id         bigint                   NOT NULL,
+    username   text                     NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    PRIMARY KEY (id)
+);
