69d1be8d37ac68c0f5a8e066a2220e08eee9e70c
eflerrr <cool.yan_sv@mail.ru>
2024-03-07 01:41:12 +0300

    MigrationTest for checking liquibase migration work

---
+++ scrapper/src/test/java/edu/eflerrr/scrapper/SimpleIntegrationTest.java
@@ -0,0 +1,48 @@
+package edu.eflerrr.scrapper;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.DirtiesContext;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DirtiesContext
+class SimpleIntegrationTest extends IntegrationTest {
+
+    @Test
+    public void MigrationTest() {
+        try (Connection connection = DriverManager.getConnection(
+            POSTGRES.getJdbcUrl(),
+            POSTGRES.getUsername(),
+            POSTGRES.getPassword()
+        )
+        ) {
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery(
+                "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+            );
+            List<String> tableNames = new ArrayList<>();
+            while (resultSet.next()) {
+                tableNames.add(resultSet.getString("table_name"));
+            }
+
+            assertThat(tableNames)
+                .hasSize(5)
+                .containsExactlyInAnyOrder(
+                    "chat", "link", "tracking",
+                    "databasechangelog", "databasechangeloglock"
+                );
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+}
