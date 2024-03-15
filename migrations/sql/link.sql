1b00eb0cbe546540ed22d6240a842e8193f265da
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:25:57 +0300

    Link table modified

--- migrations/sql/link.sql
+++ migrations/sql/link.sql
@@ -6,7 +6,8 @@ CREATE TABLE Link
     id         bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     url        text                     NOT NULL UNIQUE,
     created_at timestamp with time zone NOT NULL,
-    checked_at timestamp with time zone NOT NULL
+    checked_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL
 );
 
 -- rollback DROP TABLE Link;
