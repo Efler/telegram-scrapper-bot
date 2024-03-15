e0d8de7cfc34f8198b520c85122f7a162c25f01b
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:27:38 +0300

    Tracking table modified

--- migrations/sql/tracking.sql
+++ migrations/sql/tracking.sql
@@ -6,8 +6,8 @@ CREATE TABLE Tracking
     id      bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     chat_id bigint NOT NULL,
     link_id bigint NOT NULL,
-    FOREIGN KEY (chat_id) REFERENCES Chat (id),
-    FOREIGN KEY (link_id) REFERENCES Link (id)
+    FOREIGN KEY (chat_id) REFERENCES Chat (id) ON DELETE CASCADE,
+    FOREIGN KEY (link_id) REFERENCES Link (id) ON DELETE CASCADE
 );
 
 -- rollback DROP TABLE Tracking;
