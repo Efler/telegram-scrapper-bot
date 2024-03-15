0ab09065a8f926383b4e26de18bcd37e806054a9
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:30:03 +0300

    Chat dao-class with tests added

---
+++ scrapper/src/main/java/edu/eflerrr/scrapper/domain/dto/Chat.java
@@ -0,0 +1,26 @@
+package edu.eflerrr.scrapper.domain.dto;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+public class Chat {
+
+    private Long id;
+    private String username;
+    private OffsetDateTime createdAt;
+
+    public Chat(Long id, String username) {
+        this.id = id;
+        this.username = username;
+        this.createdAt = OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC);
+    }
+
+}
