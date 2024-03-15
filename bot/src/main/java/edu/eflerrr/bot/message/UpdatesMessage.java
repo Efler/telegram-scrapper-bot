ce35daa92b889b09ada6bfa6b26aaca22a9e6a76
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:29:24 +0300

    message package with static messages for bot added

---
+++ bot/src/main/java/edu/eflerrr/bot/message/UpdatesMessage.java
@@ -0,0 +1,44 @@
+package edu.eflerrr.bot.message;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class UpdatesMessage {
+
+    public final static String UPDATE_MESSAGE_TEMPLATE = """
+        *Новое обновление\\!*
+
+        ___Ресурс_\r__: %s
+        ___Описание_\r__: %s
+        ___Ссылка_\r__: %s
+        """;
+
+    public final static String REPOSITORY_UPDATE =
+        "Новые изменения метаданных/настроек репозитория";
+
+    public final static String REPOSITORY_PUSH =
+        "Новый коммит(ы) в репозитории";
+
+    public final static String REPOSITORY_BRANCH_CREATE =
+        "Новая ветка в репозитории";
+
+    public final static String REPOSITORY_BRANCH_DELETE =
+        "Удаление ветки в репозитории";
+
+    public final static String QUESTION_ANSWER =
+        "Новый ответ на вопрос";
+
+    public final static String QUESTION_COMMENT =
+        "Новый комментарий к вопросу/ответу";
+
+    public final static String QUESTION_ACCEPTED_ANSWER =
+        "Ответ был принят автором вопроса";
+
+    public final static String QUESTION_POST_STATE_CHANGED =
+        "Состояние вопроса было изменено";
+
+    public final static String QUESTION_UNKNOWN_UPDATE =
+        "Неизвестное обновление вопроса";
+
+}
