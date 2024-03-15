f823557e690e11c42f715455664ddf19514ee30e
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:35:41 +0300

    a part of custom exceptions added

---
+++ bot/src/main/java/edu/eflerrr/bot/exception/InvalidDataException.java
@@ -0,0 +1,9 @@
+package edu.eflerrr.bot.exception;
+
+public class InvalidDataException extends RuntimeException {
+
+    public InvalidDataException(String message) {
+        super(message);
+    }
+
+}
