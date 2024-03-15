f32573eb3b319e66ac1b8ecf5792dfba830a41d6
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:34:04 +0300

    few RestExceptionHandler's methods refactored

--- scrapper/src/main/java/edu/eflerrr/scrapper/advice/RestExceptionHandler.java
+++ scrapper/src/main/java/edu/eflerrr/scrapper/advice/RestExceptionHandler.java
@@ -3,12 +3,12 @@ package edu.eflerrr.scrapper.advice;
 import edu.eflerrr.scrapper.controller.dto.response.ApiErrorResponse;
 import edu.eflerrr.scrapper.exception.DuplicateLinkPostException;
 import edu.eflerrr.scrapper.exception.DuplicateRegistrationException;
-import edu.eflerrr.scrapper.exception.InvalidRequestDataException;
 import edu.eflerrr.scrapper.exception.LinkNotFoundException;
 import edu.eflerrr.scrapper.exception.TgChatNotExistException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -16,7 +16,7 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @RestControllerAdvice
 public class RestExceptionHandler {
 
-    @ExceptionHandler(TgChatNotExistException.class)      // TODO: stub!
+    @ExceptionHandler(TgChatNotExistException.class)
     public ResponseEntity<ApiErrorResponse> tgChatNotExist(TgChatNotExistException ex) {
         var response = new ApiErrorResponse()
             .description("Чат не существует")
@@ -29,7 +29,7 @@ public class RestExceptionHandler {
         return ResponseEntity.status(NOT_FOUND).body(response);
     }
 
-    @ExceptionHandler(LinkNotFoundException.class)      // TODO: stub!
+    @ExceptionHandler(LinkNotFoundException.class)
     public ResponseEntity<ApiErrorResponse> linkNotFound(LinkNotFoundException ex) {
         var response = new ApiErrorResponse()
             .description("Ссылка не найдена")
@@ -42,8 +42,8 @@ public class RestExceptionHandler {
         return ResponseEntity.status(NOT_FOUND).body(response);
     }
 
-    @ExceptionHandler(InvalidRequestDataException.class)      // TODO: stub!
-    public ResponseEntity<ApiErrorResponse> invalidRequestData(InvalidRequestDataException ex) {
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class, NullPointerException.class})
+    public ResponseEntity<ApiErrorResponse> invalidRequestData(Exception ex) {
         var response = new ApiErrorResponse()
             .description("Некорректные параметры запроса")
             .code(Integer.toString(BAD_REQUEST.value()))
@@ -55,7 +55,7 @@ public class RestExceptionHandler {
         return ResponseEntity.status(BAD_REQUEST).body(response);
     }
 
-    @ExceptionHandler(DuplicateRegistrationException.class)      // TODO: stub!
+    @ExceptionHandler(DuplicateRegistrationException.class)
     public ResponseEntity<ApiErrorResponse> duplicateRegistration(DuplicateRegistrationException ex) {
         var response = new ApiErrorResponse()
             .description("Повторная регистрация")
@@ -68,7 +68,7 @@ public class RestExceptionHandler {
         return ResponseEntity.status(CONFLICT).body(response);
     }
 
-    @ExceptionHandler(DuplicateLinkPostException.class)      // TODO: stub!
+    @ExceptionHandler(DuplicateLinkPostException.class)
     public ResponseEntity<ApiErrorResponse> duplicateLinkPost(DuplicateLinkPostException ex) {
         var response = new ApiErrorResponse()
             .description("Повторное добавление ссылки")
