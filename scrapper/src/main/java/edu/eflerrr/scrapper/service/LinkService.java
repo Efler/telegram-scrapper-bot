59fd23ea92010966a74ee88adeb474505d52ea53
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:37:26 +0300

    LinkService with jdbc-implementation done

---
+++ scrapper/src/main/java/edu/eflerrr/scrapper/service/LinkService.java
@@ -0,0 +1,15 @@
+package edu.eflerrr.scrapper.service;
+
+import edu.eflerrr.scrapper.domain.dto.Link;
+import java.net.URI;
+import java.util.List;
+
+public interface LinkService {
+
+    Link add(long tgChatId, URI url);
+
+    Link delete(long tgChatId, URI url);
+
+    List<Link> listAll(long tgChatId);
+
+}
