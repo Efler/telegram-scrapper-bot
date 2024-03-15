ec7c348ecac086481fe37545f826f4f130cf5487
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:42:25 +0300

    logic for endpoints using services done

--- bot/src/main/java/edu/eflerrr/bot/controller/UpdatesController.java
+++ bot/src/main/java/edu/eflerrr/bot/controller/UpdatesController.java
@@ -2,12 +2,15 @@ package edu.eflerrr.bot.controller;
 
 import edu.eflerrr.bot.controller.dto.request.LinkUpdate;
 import edu.eflerrr.bot.controller.dto.response.ApiErrorResponse;
+import edu.eflerrr.bot.service.UpdatesService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.net.MalformedURLException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,7 +22,10 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @Validated
 @Tag(name = "updates", description = "the updates API")
 @RestController
+@RequiredArgsConstructor
 public class UpdatesController {
+
+    private final UpdatesService updatesService;
 
     /**
      * POST /updates : Отправить обновление
@@ -50,8 +56,14 @@ public class UpdatesController {
                    required = true)
         @RequestBody
         LinkUpdate linkUpdate
-    ) {
-        return ResponseEntity.ok().build();     // TODO: stub!
+    ) throws MalformedURLException {
+        updatesService.processUpdate(
+            linkUpdate.getId(),
+            linkUpdate.getUrl().toURL(),
+            linkUpdate.getDescription(),
+            linkUpdate.getTgChatIds()
+        );
+        return ResponseEntity.ok().build();
     }
 
 }
