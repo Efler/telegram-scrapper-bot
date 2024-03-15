b958160683fac0a9820e38983b2a40f6700c45ed
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:33:03 +0300

    changed parent class for exceptions

--- scrapper/src/main/java/edu/eflerrr/scrapper/exception/LinkNotFoundException.java
+++ scrapper/src/main/java/edu/eflerrr/scrapper/exception/LinkNotFoundException.java
@@ -1,6 +1,6 @@
 package edu.eflerrr.scrapper.exception;
 
-public class LinkNotFoundException extends Exception {
+public class LinkNotFoundException extends RuntimeException {
     public LinkNotFoundException(String message) {
         super(message);
     }
