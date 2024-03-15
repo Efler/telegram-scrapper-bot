8b96aaff4b07c3e6678b9e6ed7cf89efaf0c8030
eflerrr <cool.yan_sv@mail.ru>
2024-03-16 01:30:27 +0300

    Tracking dao-class with tests added

---
+++ scrapper/src/main/java/edu/eflerrr/scrapper/domain/dto/Tracking.java
@@ -0,0 +1,24 @@
+package edu.eflerrr.scrapper.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+public class Tracking {
+
+    private Long id;
+    private Long chatId;
+    private Long linkId;
+
+    public Tracking(Long chatId, Long linkId) {
+        this.id = null;
+        this.chatId = chatId;
+        this.linkId = linkId;
+    }
+
+}
