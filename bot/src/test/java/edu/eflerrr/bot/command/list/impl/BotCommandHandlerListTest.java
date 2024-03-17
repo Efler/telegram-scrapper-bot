ac00f7e3e24a9d25e2125f0eae0c1d38c0226ffd
eflerrr <cool.yan_sv@mail.ru>
2024-03-17 23:29:34 +0300

    refactoring due to new command /remove_me

--- bot/src/test/java/edu/eflerrr/bot/command/list/impl/BotCommandHandlerListTest.java
+++ bot/src/test/java/edu/eflerrr/bot/command/list/impl/BotCommandHandlerListTest.java
@@ -3,54 +3,44 @@ package edu.eflerrr.bot.command.list.impl;
 import edu.eflerrr.bot.command.handler.CommandHandler;
 import edu.eflerrr.bot.command.handler.impl.HelpCommandHandler;
 import edu.eflerrr.bot.command.handler.impl.ListCommandHandler;
+import edu.eflerrr.bot.command.handler.impl.RemoveMeCommandHandler;
 import edu.eflerrr.bot.command.handler.impl.StartCommandHandler;
 import edu.eflerrr.bot.command.handler.impl.TrackCommandHandler;
 import edu.eflerrr.bot.command.handler.impl.UntrackCommandHandler;
 import edu.eflerrr.bot.command.list.CommandHandlerList;
-import edu.eflerrr.bot.configuration.ApplicationConfig;
-import edu.eflerrr.bot.repository.BotRepository;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ApplicationContext;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
 class BotCommandHandlerListTest {
-    /*
     private final List<CommandHandler> commands;
 
     private final CommandHandlerList botCommandHandlerListTest;
 
     public BotCommandHandlerListTest() {
-        var startCommandHandler = new StartCommandHandler(
-            mock(BotRepository.class)
-        );
-        var helpCommandHandler = new HelpCommandHandler(
-            mock(ApplicationContext.class)
-        );
-        var listCommandHandler = new ListCommandHandler(
-            mock(BotRepository.class)
-        );
-        var trackCommandHandler = new TrackCommandHandler(
-            mock(BotRepository.class),
-            mock(ApplicationConfig.class)
-        );
-        var untrackCommandHandler = new UntrackCommandHandler(
-            mock(BotRepository.class)
-        );
+        var startCommandHandler = mock(StartCommandHandler.class);
+        var helpCommandHandler = mock(HelpCommandHandler.class);
+        var listCommandHandler = mock(ListCommandHandler.class);
+        var trackCommandHandler = mock(TrackCommandHandler.class);
+        var untrackCommandHandler = mock(UntrackCommandHandler.class);
+        var removeMeCommandHandler = mock(RemoveMeCommandHandler.class);
+
         botCommandHandlerListTest = new BotCommandHandlerList(
             startCommandHandler,
             helpCommandHandler,
             trackCommandHandler,
             untrackCommandHandler,
-            listCommandHandler
+            listCommandHandler,
+            removeMeCommandHandler
         );
         commands = List.of(
             startCommandHandler,
             helpCommandHandler,
             trackCommandHandler,
             untrackCommandHandler,
-            listCommandHandler
+            listCommandHandler,
+            removeMeCommandHandler
         );
     }
 
@@ -61,5 +51,4 @@ class BotCommandHandlerListTest {
         assertThat(actualCommandList)
             .isEqualTo(commands);
     }
-    */
 }
