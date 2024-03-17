d9f5b44a5a443ee4687d46237248d9555eaabf8a
eflerrr <cool.yan_sv@mail.ru>
2024-03-17 23:44:30 +0300

    new exceptions added

---
+++ bot/src/main/java/edu/eflerrr/bot/exception/LinkNotFoundException.java
@@ -0,0 +1,7 @@
+package edu.eflerrr.bot.exception;
+
+public class LinkNotFoundException extends RuntimeException {
+    public LinkNotFoundException(String message) {
+        super(message);
+    }
+}
