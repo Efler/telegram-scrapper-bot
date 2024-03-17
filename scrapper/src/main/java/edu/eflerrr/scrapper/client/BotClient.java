134d1ef11aeea526470e90de99b9e0c31ea17778
eflerrr <cool.yan_sv@mail.ru>
2024-03-17 23:28:55 +0300

    fixing a bug

--- scrapper/src/main/java/edu/eflerrr/scrapper/client/BotClient.java
+++ scrapper/src/main/java/edu/eflerrr/scrapper/client/BotClient.java
@@ -2,14 +2,16 @@ package edu.eflerrr.scrapper.client;
 
 import edu.eflerrr.scrapper.client.dto.request.SendUpdateRequest;
 import edu.eflerrr.scrapper.client.dto.response.BotErrorResponse;
+import edu.eflerrr.scrapper.exception.InvalidDataException;
 import java.net.URI;
 import java.util.List;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class BotClient {
     private final WebClient webClient;
-    public final String defaultApiUrl = "localhost:8090";
+    public final String defaultApiUrl = "http://localhost:8090";
 
     public BotClient(String baseApiUrl) {
         this.webClient = WebClient.create(
@@ -31,6 +33,9 @@ public class BotClient {
             })
             .block();
         if (botResponse != null) {
+            if (Integer.parseInt(botResponse.code()) == BAD_REQUEST.value()) {
+                throw new InvalidDataException(botResponse.description());
+            }
             throw new RuntimeException(
                 "Error occurred during sendUpdate in BotClient! ErrorResponse: " + botResponse
             );
