be7d26066ac5d516692d8a08d510fd468c1a400f
eflerrr <cool.yan_sv@mail.ru>
2024-03-17 23:43:56 +0300

    refactoring

--- bot/src/main/java/edu/eflerrr/bot/client/ScrapperClient.java
+++ bot/src/main/java/edu/eflerrr/bot/client/ScrapperClient.java
@@ -4,20 +4,20 @@ import edu.eflerrr.bot.client.dto.request.LinkRequest;
 import edu.eflerrr.bot.client.dto.response.LinkResponse;
 import edu.eflerrr.bot.client.dto.response.ListLinksResponse;
 import edu.eflerrr.bot.client.dto.response.ScrapperErrorResponse;
+import edu.eflerrr.bot.exception.DuplicateLinkPostException;
 import edu.eflerrr.bot.exception.DuplicateRegistrationException;
 import edu.eflerrr.bot.exception.InvalidDataException;
+import edu.eflerrr.bot.exception.LinkNotFoundException;
+import edu.eflerrr.bot.exception.TgChatNotExistException;
 import java.net.URI;
+import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.CONFLICT;
-
-// TODO! доделать исключения в clients
-// TODO! допилить коммуникацию между сервисами
-// TODO! починить тесты
-// TODO! возможно стоит добавить логирование
-// TODO! возможно стоит добавить валидацию ссылок
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
 
 @SuppressWarnings("MemberName")
 public class ScrapperClient {
@@ -25,7 +25,13 @@ public class ScrapperClient {
     private static final String TG_CHAT_ENDPOINT = "/tg-chat";
     private static final String LINKS_ENDPOINT = "/links";
     private final WebClient webClient;
-    public final String defaultApiUrl = "localhost:8080";
+    private final String trackLinkErrorMessage =
+        "Error occurred during trackLink in ScrapperClient! ErrorResponse: ";
+    private final String untrackLinkErrorMessage =
+        "Error occurred during untrackLink in ScrapperClient! ErrorResponse: ";
+    private final String listLinksErrorMessage =
+        "Error occurred during listLinks in ScrapperClient! ErrorResponse: ";
+    public final String defaultApiUrl = "http://localhost:8080";
 
     public ScrapperClient(String baseApiUrl) {
         this.webClient = WebClient.create(
@@ -35,20 +41,22 @@ public class ScrapperClient {
     }
 
     public void registerTgChat(Long id) {
+        AtomicInteger statusCode = new AtomicInteger();
         var botResponse = webClient.post()
             .uri(TG_CHAT_ENDPOINT + "/" + id)
             .exchangeToMono(response -> {
                 if (!response.statusCode().is2xxSuccessful()) {
+                    statusCode.set(response.statusCode().value());
                     return response.bodyToMono(ScrapperErrorResponse.class);
                 }
                 return Mono.empty();
             })
             .block();
         if (botResponse != null) {
-            if (Integer.parseInt(botResponse.code()) == CONFLICT.value()) {
+            if (statusCode.get() == (CONFLICT.value())) {
                 throw new DuplicateRegistrationException(botResponse.description());
             }
-            if (Integer.parseInt(botResponse.code()) == BAD_REQUEST.value()) {
+            if (statusCode.get() == (BAD_REQUEST.value())) {
                 throw new InvalidDataException(botResponse.description());
             }
             throw new RuntimeException(
@@ -58,16 +66,24 @@ public class ScrapperClient {
     }
 
     public void deleteTgChat(Long id) {
+        AtomicInteger statusCode = new AtomicInteger();
         var botResponse = webClient.delete()
             .uri(TG_CHAT_ENDPOINT + "/" + id)
             .exchangeToMono(response -> {
                 if (!response.statusCode().is2xxSuccessful()) {
+                    statusCode.set(response.statusCode().value());
                     return response.bodyToMono(ScrapperErrorResponse.class);
                 }
                 return Mono.empty();
             })
             .block();
         if (botResponse != null) {
+            if (statusCode.get() == (NOT_FOUND.value())) {
+                throw new TgChatNotExistException(botResponse.description());
+            }
+            if (statusCode.get() == (BAD_REQUEST.value())) {
+                throw new InvalidDataException(botResponse.description());
+            }
             throw new RuntimeException(
                 "Error occurred during deleteTgChat in ScrapperClient! ErrorResponse: " + botResponse
             );
@@ -80,13 +96,31 @@ public class ScrapperClient {
             .header(TG_CHAT_ID_HEADER, id.toString())
             .bodyValue(new LinkRequest(url))
             .exchangeToMono(response -> {
-                if (response.statusCode().is2xxSuccessful()) {
+                if (response.statusCode().equals(OK)) {
                     return response.bodyToMono(LinkResponse.class);
                 }
-                var errorResponse = response.bodyToMono(ScrapperErrorResponse.class);
-                return Mono.error(new RuntimeException(
-                    "Error occurred during trackLink in ScrapperClient! ErrorResponse: " + errorResponse
-                ));
+                return response.bodyToMono(ScrapperErrorResponse.class)
+                    .flatMap(errorResponse -> {
+                        String errorMessage = errorResponse.description();
+                        if (response.statusCode().value() == BAD_REQUEST.value()) {
+                            return Mono.error(new InvalidDataException(
+                                trackLinkErrorMessage + errorMessage
+                            ));
+                        }
+                        if (response.statusCode().value() == CONFLICT.value()) {
+                            return Mono.error(new DuplicateLinkPostException(
+                                trackLinkErrorMessage + errorMessage
+                            ));
+                        }
+                        if (response.statusCode().value() == NOT_FOUND.value()) {
+                            return Mono.error(new TgChatNotExistException(
+                                trackLinkErrorMessage + errorMessage
+                            ));
+                        }
+                        return Mono.error(new RuntimeException(
+                            trackLinkErrorMessage + errorMessage
+                        ));
+                    });
             })
             .block();
     }
@@ -97,30 +131,63 @@ public class ScrapperClient {
             .header(TG_CHAT_ID_HEADER, id.toString())
             .bodyValue(new LinkRequest(url))
             .exchangeToMono(response -> {
-                if (response.statusCode().is2xxSuccessful()) {
+                if (response.statusCode().equals(OK)) {
                     return response.bodyToMono(LinkResponse.class);
                 }
-                var errorResponse = response.bodyToMono(ScrapperErrorResponse.class);
-                return Mono.error(new RuntimeException(
-                    "Error occurred during untrackLink in ScrapperClient! ErrorResponse: " + errorResponse
-                ));
+                return response.bodyToMono(ScrapperErrorResponse.class)
+                    .flatMap(errorResponse -> {
+                        String errorMessage = errorResponse.description();
+                        if (response.statusCode().value() == BAD_REQUEST.value()) {
+                            return Mono.error(new InvalidDataException(
+                                untrackLinkErrorMessage + errorMessage
+                            ));
+                        }
+                        if (response.statusCode().value() == NOT_FOUND.value()) {
+                            if (errorMessage.equals("Чат не существует")) {
+                                return Mono.error(new TgChatNotExistException(
+                                    untrackLinkErrorMessage + errorMessage
+                                ));
+                            } else {
+                                return Mono.error(new LinkNotFoundException(
+                                    untrackLinkErrorMessage + errorMessage
+                                ));
+                            }
+                        }
+                        return Mono.error(new RuntimeException(
+                            untrackLinkErrorMessage + errorMessage
+                        ));
+                    });
             })
             .block();
     }
 
     public ListLinksResponse listLinks(Long id) {
-        return webClient.post()
+        return webClient.get()
             .uri(LINKS_ENDPOINT)
             .header(TG_CHAT_ID_HEADER, id.toString())
             .exchangeToMono(response -> {
-                if (response.statusCode().is2xxSuccessful()) {
+                if (response.statusCode().equals(OK)) {
                     return response.bodyToMono(ListLinksResponse.class);
                 }
-                var errorResponse = response.bodyToMono(ScrapperErrorResponse.class);
-                return Mono.error(new RuntimeException(
-                    "Error occurred during listLinks in ScrapperClient! ErrorResponse: " + errorResponse
-                ));
+                return response.bodyToMono(ScrapperErrorResponse.class)
+                    .flatMap(errorResponse -> {
+                        String errorMessage = errorResponse.description();
+                        if (response.statusCode().value() == BAD_REQUEST.value()) {
+                            return Mono.error(new InvalidDataException(
+                                listLinksErrorMessage + errorMessage
+                            ));
+                        }
+                        if (response.statusCode().value() == NOT_FOUND.value()) {
+                            return Mono.error(new TgChatNotExistException(
+                                listLinksErrorMessage + errorMessage
+                            ));
+                        }
+                        return Mono.error(new RuntimeException(
+                            listLinksErrorMessage + errorMessage
+                        ));
+                    });
             })
             .block();
     }
+
 }
