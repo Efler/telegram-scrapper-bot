17abe6f7dc63d4aedf1354387d945b255d31b320
eflerrr <cool.yan_sv@mail.ru>
2024-03-17 23:41:54 +0300

    new exceptions added

---
+++ bot/src/main/java/edu/eflerrr/bot/exception/DuplicateLinkPostException.java
@@ -0,0 +1,7 @@
+package edu.eflerrr.bot.exception;
+
+public class DuplicateLinkPostException extends RuntimeException {
+    public DuplicateLinkPostException(String message) {
+        super(message);
+    }
+}
