d677d68cc8056e9304bdc7f3bf625cff961f181d
eflerrr <cool.yan_sv@mail.ru>
2024-03-17 23:38:48 +0300

    liquibase scripts refactored due to jooq usage

--- migrations/sql/chat.sql
+++ migrations/sql/chat.sql
@@ -1,12 +1,12 @@
 -- liquibase formatted sql
 
 -- changeset eflerrr:init_chat_table
-CREATE TABLE Chat
+CREATE TABLE "Chat"
 (
-    id         bigint                   NOT NULL,
-    username   text                     NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    PRIMARY KEY (id)
+    "id"         bigint                   NOT NULL,
+    "username"   text                     NOT NULL,
+    "created_at" timestamp with time zone NOT NULL,
+    PRIMARY KEY ("id")
 );
 
--- rollback DROP TABLE Chat;
+-- rollback DROP TABLE "Chat";
