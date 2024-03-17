be5e715c8780a2ed6b2abc809bea7120fc06b09e
eflerrr <cool.yan_sv@mail.ru>
2024-03-17 23:39:57 +0300

    Markdown style checker added

--- bot/src/main/java/edu/eflerrr/bot/command/handler/impl/HelpCommandHandler.java
+++ bot/src/main/java/edu/eflerrr/bot/command/handler/impl/HelpCommandHandler.java
@@ -3,6 +3,7 @@ package edu.eflerrr.bot.command.handler.impl;
 import com.pengrad.telegrambot.model.Update;
 import edu.eflerrr.bot.command.handler.CommandHandler;
 import edu.eflerrr.bot.command.list.CommandHandlerList;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -36,6 +37,13 @@ public class HelpCommandHandler implements CommandHandler {
         return command != null && command.equals(name);
     }
 
+    public String toMarkdownStyle(String word) {
+        return word.replaceAll(
+            "([" + Pattern.quote("_*[]()~`><#+-=|{}.!") + "])",
+            "\\\\$1"
+        );
+    }
+
     @Override
     public String handle(Update update) {
         if (!checkFormat(update.message().text())) {
@@ -46,7 +54,12 @@ public class HelpCommandHandler implements CommandHandler {
             return HELP_COMMAND_EMPTY_LIST_ERROR;
         } else {
             return HELP_COMMAND_SUCCESS_HEADER + commandHandlers.stream()
-                .map((h) -> String.format("*%s* \\-\\> _%s_", h.getCommandName(), h.getCommandDescription()))
+                .map((h) -> String.format(
+                        "*%s* \\-\\> _%s_",
+                        toMarkdownStyle(h.getCommandName()),
+                        toMarkdownStyle(h.getCommandDescription())
+                    )
+                )
                 .collect(Collectors.joining("\n"));
         }
     }
