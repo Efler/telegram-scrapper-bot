90da9319b3c542b4db32c25942f383448ca89fd3
eflerrr <cool.yan_sv@mail.ru>
2024-03-17 23:41:49 +0300

    new exceptions added

---
+++ scrapper/src/main/java/edu/eflerrr/scrapper/exception/InvalidDataException.java
@@ -0,0 +1,7 @@
+package edu.eflerrr.scrapper.exception;
+
+public class InvalidDataException extends RuntimeException {
+    public InvalidDataException(String message) {
+        super(message);
+    }
+}
