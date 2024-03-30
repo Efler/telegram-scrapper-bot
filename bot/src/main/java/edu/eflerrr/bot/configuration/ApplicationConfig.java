dc6044265baec047c9ac33daae555590813fe71b
eflerrr <cool.yan_sv@mail.ru>
2024-03-30 21:16:47 +0300

    added retry-configuring properties for clients + rebasing readme commits from main

--- bot/src/main/java/edu/eflerrr/bot/configuration/ApplicationConfig.java
+++ bot/src/main/java/edu/eflerrr/bot/configuration/ApplicationConfig.java
@@ -2,7 +2,9 @@ package edu.eflerrr.bot.configuration;
 
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import java.time.Duration;
 import java.util.List;
+import java.util.Set;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
 
@@ -14,10 +16,37 @@ public record ApplicationConfig(
     @NotEmpty
     List<String> availableSites,
     @NotNull
-    Api api
+    Api api,
+    @NotNull
+    Retry retry
 ) {
     public record Api(
         String scrapperBaseUrl
     ) {
     }
+
+    public record Retry(
+        @NotNull
+        ClientRetryProperties scrapperClient
+    ) {
+        public record ClientRetryProperties(
+            @NotNull
+            Boolean enable,
+            @NotNull
+            Set<Integer> retryStatusCodes,
+            @NotNull
+            BackoffPolicy backoffPolicy,
+            @NotNull
+            Integer maxAttempts,
+            @NotNull
+            Duration initialInterval,
+            @NotNull
+            Double multiplier
+        ) {
+            public enum BackoffPolicy {
+                CONSTANT, LINEAR, EXPONENTIAL
+            }
+        }
+    }
+
 }
