2d238242d2d77f89f5408f0ce387f9bc83425410
eflerrr <cool.yan_sv@mail.ru>
2024-04-05 00:04:49 +0300

    making a flexible 'update sender' + rebasing code review changes from homework-5-with-bonus

--- scrapper/src/main/java/edu/eflerrr/scrapper/client/BotClient.java
+++ scrapper/src/main/java/edu/eflerrr/scrapper/client/BotClient.java
@@ -4,6 +4,7 @@ import edu.eflerrr.scrapper.client.dto.request.SendUpdateRequest;
 import edu.eflerrr.scrapper.client.dto.response.BotErrorResponse;
 import edu.eflerrr.scrapper.exception.InvalidDataException;
 import edu.eflerrr.scrapper.exception.retry.RetryableRequestException;
+import edu.eflerrr.scrapper.service.UpdateSender;
 import java.net.URI;
 import java.util.List;
 import java.util.Set;
@@ -12,7 +13,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-public class BotClient {
+public class BotClient implements UpdateSender {
     private final WebClient webClient;
     private final RetryTemplate retryTemplate;
     private final Set<Integer> retryStatusCodes;
@@ -52,6 +53,7 @@ public class BotClient {
             });
     }
 
+    @Override
     public void sendUpdate(Long id, URI url, String description, List<Long> tgChatIds) {
         var botResponse = sendUpdateWithRetry(
             new SendUpdateRequest(id, url, description, tgChatIds)
